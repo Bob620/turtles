6a38ba66348bb29740101dcc8dca036e7edde8fa
bob620 <bruder.kraft225@gmail.com>
2020-08-28 14:37:51 -0500

    initial commit

---
+++ farm.lua
@@ -0,0 +1,16 @@
+select(2);
+
+turtle.forward();
+exists, data = turtle.inspectDown();
+
+if exists then
+    if data["state"]["age"] == 3 then
+        turtle.digDown();
+        turtle.placeDown();
+    end
+else
+    turtle.digDown();
+    turtle.placeDown();
+end
+
+turtle.back();
