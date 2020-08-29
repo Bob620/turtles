5f8241465a31ff1a4e5010a4fed39ad720f0e58d
bob620 <bruder.kraft225@gmail.com>
2020-08-29 02:45:56 -0500

    initial commit

--- turtle/fuel.lua
+++ turtle/fuel.lua
@@ -2,42 +2,49 @@
 
 -- file = fs.open("file.lua", "w"); file.write(http.get("url").readAll()); file.close();
 
+function updateFuelScreen(fuelPercent)
+    local termWidth, termHight = term.getSize();
+    term.clear();
+
+    paintutils.drawPixel(termWidth - 1, 1, colors.white);
+
+    paintutils.drawPixel(termWidth - 2, 2, colors.white);
+    paintutils.drawPixel(termWidth - 1, 2, colors.white);
+    paintutils.drawPixel(termWidth, 2, colors.white);
+
+    for i = 3, termHight - 1, 1 do
+        paintutils.drawPixel(termWidth - 2, i, colors.white);
+        paintutils.drawPixel(termWidth, i, colors.white);
+
+        if fuelPercent >= (termHight - i) / termHight then
+            paintutils.drawPixel(termWidth - 1, i, colors.blue);
+        else
+            paintutils.drawPixel(termWidth - 1, i, colors.black);
+        end
+    end
+
+    paintutils.drawPixel(termWidth - 2, termHight, colors.white);
+    paintutils.drawPixel(termWidth - 1, termHight, colors.white);
+    paintutils.drawPixel(termWidth, termHight, colors.white);
+
+    term.setCursorPos(1, 1);
+end
+
 turtle.select(1);
 
 local maxFuel = turtle.getFuelLimit();
 local fuel = turtle.getFuelLevel();
 local fuelPercent = fuel / maxFuel;
-if fuelPercent < 40 then
-    while fuelPercent < 80 do
+updateFuelScreen();
+
+if fuelPercent < .40 then
+    while fuelPercent < .80 do
         turtle.suckDown(64);
         turtle.refuel();
         fuel = turtle.getFuelLevel();
         fuelPercent = fuel / maxFuel;
+        updateFuelScreen(fuelPercent);
     end
 end
 
-local termWidth, termHight = term.getSize();
-term.clear();
-
-paintutils.drawPixel(termWidth - 1, 1, colors.white);
-
-paintutils.drawPixel(termWidth - 2, 2, colors.white);
-paintutils.drawPixel(termWidth - 1, 2, colors.white);
-paintutils.drawPixel(termWidth, 2, colors.white);
-
-for i = 3, termHight - 1, 1 do
-    paintutils.drawPixel(termWidth - 2, i, colors.white);
-    paintutils.drawPixel(termWidth, i, colors.white);
-
-    if fuelPercent >= (termHight - i) / termHight then
-        paintutils.drawPixel(termWidth - 1, i, colors.blue);
-    else
-        paintutils.drawPixel(termWidth - 1, i, colors.black);
-    end
-end
-
-paintutils.drawPixel(termWidth - 2, termHight, colors.white);
-paintutils.drawPixel(termWidth - 1, termHight, colors.white);
-paintutils.drawPixel(termWidth, termHight, colors.white);
-
-term.setCursorPos(1, 1);
+updateFuelScreen();
