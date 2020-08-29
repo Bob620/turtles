7379ffbe9f3d6f32f0cdd3a85644ac00f9c1f02d
bob620 <bruder.kraft225@gmail.com>
2020-08-29 02:19:07 -0500

    initial commit

--- turtle/fuel.lua
+++ turtle/fuel.lua
@@ -6,13 +6,13 @@ turtle.select(1);
 
 local maxFuel = turtle.getFuelLimit();
 local fuel = turtle.getFuelLevel();
-local fuelPercent = (maxFuel / fuel) * 100;
+local fuelPercent = (fuel / maxFuel) * 100;
 if fuelPercent < 80 then
     turtle.refuel();
 end
 
 fuel = turtle.getFuelLevel();
-fuelPercent = (maxFuel / fuel) * 100;
+fuelPercent = (fuel / maxFuel) * 100;
 
 local termWidth, termHight = term.getSize();
 
@@ -26,7 +26,7 @@ for i = 3, termHight - 1, 1 do
     paintutils.drawPixel(termWidth - 2, i, colors.white);
     paintutils.drawPixel(termWidth, i, colors.white);
 
-    if fuelPercent >= termHight / (termHight - i) then
+    if fuelPercent >= (termHight - i) / termHight then
         paintutils.drawPixel(termWidth - 1, i, colors.blue);
     else
         paintutils.drawPixel(termWidth - 1, i, colors.black);
